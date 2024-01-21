cc779d177a4a41d74845b983e86ddc6bac951c4e
Naman Tripathi <namantripathi@Namans-MacBook-Air.local>
2024-01-21 16:54:54 +0530

    data

---
+++ 19_jan_2024_11.06Am.js
@@ -0,0 +1,68 @@
+const SentimentData = {
+    'Bitcoin': {
+  positive: 57.48,
+  negative: 20.52,
+  neutral: 22.00},
+    'Ethereum': { 
+  positive: 75.14,
+  negative: 14.86,
+  neutral: 10.00},
+    'Binance Coin': { 
+  positive: 70.34,
+  negative: 19.66,
+  neutral: 10.00},
+    'Tether': { 
+  positive: 65.23,
+  negative: 24.77,
+  neutral: 10.00
+ },
+    'Solana': { 
+  positive: 72.34,
+  negative: 17.66,
+  neutral: 10.00
+    },
+    'Cardano': {
+  positive: 76.34,
+  negative: 13.66,
+  neutral: 10.00 },
+    'XRP': {
+  positive: 65.34,
+  negative: 24.66,
+  neutral: 10.00 },
+    'Polkadot': { 
+  positive: 64.22,
+  negative: 21.34,
+  neutral: 14.44 },
+    'Dogecoin': {
+  positive: 65.47,
+  negative: 22.36,
+  neutral: 12.17},
+    'Shiba Inu': {
+  positive: 56.14,
+  negative: 28.73,
+  neutral: 15.13},
+    'Avalanche': { 
+  positive: 51.24,
+  negative: 28.76,
+  neutral: 20.00},
+    'Chainlink': {
+  positive: 62.47,
+  negative: 24.53,
+  neutral: 13.00},
+    'Litecoin': { 
+  positive: 45.34,
+  negative: 39.66,
+  neutral: 15.00},
+    'Polygon': {
+  positive: 35.24,
+  negative: 49.76,
+  neutral: 15.00
+    },
+    'Stellar': {
+  positive: 68.57,
+  negative: 17.43,
+  neutral: 14.00
+    },
+
+  };
+  export default SentimentData;  
