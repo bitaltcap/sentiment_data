a4fce4b591d387361073ba1a4ac6cf5987753082
BitAlt Capital <155447900+bitaltcap@users.noreply.github.com>
2024-02-18 14:14:24 +0530

    data

---
+++ 18_Feb_2024_2.14PM.js
@@ -0,0 +1,66 @@
+const SentimentData = {
+    'Bitcoin': {
+      positive: 62.24,
+      negative: 18.76,
+      neutral: 19.00},
+    'Ethereum': {
+      positive: 61.55,
+      negative: 18.22,
+      neutral: 20.23},
+    'Binance Coin': { 
+      positive: 60.00,
+      negative: 20.00,
+      neutral: 20.00},
+    'Tether': {
+      positive: 40.00,
+      negative: 40.00,
+      neutral: 20.00},
+    'Solana': { 
+      positive: 60.00,
+      negative: 20.00,
+      neutral: 20.00},
+    'Cardano': {
+      positive: 40.00,
+      negative: 30.00,
+      neutral: 30.00},
+    'XRP': {
+      positive: 20.00,
+      negative: 70.00,
+      neutral: 10.00},
+    'Polkadot': {
+      positive: 40.00,
+      negative: 30.00,
+      neutral: 30.00},
+    'Dogecoin': {
+      positive: 35.00,
+      negative: 45.00,
+      neutral: 20.00},
+    'Shiba Inu': { 
+      positive: 80.00,
+      negative: 10.00,
+      neutral: 10.00},
+    'Avalanche': {
+      positive: 70.00,
+      negative: 15.00,
+      neutral: 15.00},
+    'Chainlink': {
+      positive: 30.00,
+      negative: 60.00,
+      neutral: 10.00},
+    'Litecoin': {
+      positive: 30.00,
+      negative: 40.00,
+      neutral: 30.00},
+    'Polygon': {
+      positive: 60.00,
+      negative: 20.00,
+      neutral: 20.00
+    },
+    'Stellar': {
+      positive: 45.00,
+      negative: 36.00,
+      neutral: 20.00
+    },
+
+  };
+  export default SentimentData;  
